ebea0916fa79e9ece2580b679d69a073fbfbe92a
Johann Doell (jsd090) <jsd090@mail.usask.ca>
2020-01-25 23:25:36 -0600

    Built a prototype for the theoretical dodge mechanic.

--- Unity/Assets/Resources/SpikePrototypeScrips/Combat Mechanics Test/Player.cs
+++ Unity/Assets/Resources/SpikePrototypeScrips/Combat Mechanics Test/Player.cs
@@ -7,6 +7,16 @@ public class Player : MonoBehaviour
     // Variables
     public float movementSpeed;
 
+    [Header("Dodge Variables")]
+    private bool dodging;
+    [SerializeField]
+    private string dodgeKey = "space";
+    [SerializeField]
+    private float dodgeSpeedMultiplication = 3f;
+    [SerializeField]
+    private float dodgeLength = 0.3f;
+
+    [Header("Object References")]
     public Rigidbody2D rb;
     public Camera mainCam;
 
@@ -26,18 +36,57 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        GetMovementData();
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        DoMovement();
+    }
 
+    void GetMovementData()
+    {
+        if (Input.GetKeyDown(dodgeKey) && !dodging && movement != Vector2.zero)
+        {
+            //Debug.Log("Dodge key pressed.");
+            dodging = true;
+            StartCoroutine(EndDash());
+        }
+
+        if (!dodging)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void DoMovement()
+    {
+
+
+        // Move character
+        if (!dodging)
+        {
+            rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            float step = movementSpeed * 3f;
+            rb.MovePosition(rb.position + movement * step * Time.fixedDeltaTime);
+        }
+
+        // Point to mouse
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
         rb.rotation = angle - 90f;
+    }
+
+    IEnumerator EndDash()
+    {
+        yield return new WaitForSeconds(0.3f);
+        //Debug.Log("Dodge end.");
+        dodging = false;
     }
 }
