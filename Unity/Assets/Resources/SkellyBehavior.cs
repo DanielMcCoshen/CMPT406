1f9ef1423ebddbf7c711dc959d450df566478f5c
Adam Soltys <ads576@mail.usask.ca>
2020-02-27 00:22:16 -0600

    Major changes to skeleton behavior, going with states now.

--- Unity/Assets/Resources/SkellyBehavior.cs
+++ Unity/Assets/Resources/SkellyBehavior.cs
@@ -4,54 +4,108 @@ using UnityEngine;
 
 public class SkellyBehavior : MonoBehaviour
 {
+    /*
     [SerializeField]
     private int jumpTimer;
-    private bool waitingForJump;
+
     public GameObject player;
     public Rigidbody2D rb;
+
     [SerializeField]
-    public float speed;
+    private float speed;
 
+    private Vector2 destination = new Vector2();
+    private bool jumping = false;
+    private float distanceToPlayer;
 
-
-    private void Jump(GameObject player)
-    {
-        //Debug.Log("JUMP");
-
-        // Where the skelly is going to jump to
-        
-        var destination_x = player.transform.position.x;
-        var destination_y = player.transform.position.y;
-
-        Vector2 direction_force = new Vector2();
-        direction_force.Set(destination_x, destination_y);
-
-        transform.position = Vector2.MoveTowards(transform.position, direction_force, speed);
-
-    }
 
     private IEnumerator JumpCheck()
     {
-        waitingForJump = true;
+        jumping = true;
         yield return new WaitForSeconds(jumpTimer);
-        Jump(player);
-        waitingForJump = false;
+        destination.Set(player.transform.position.x, player.transform.position.y);
+        Debug.Log("setting destination");
+        jumping = true;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        waitingForJump = false;
+        jumping = false;
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!waitingForJump)
+        Debug.Log(jumping);
+
+        if (!jumping)
         {
             StartCoroutine(JumpCheck());
-            Debug.Log("Ran JumpCheck");
+            //Debug.Log("Ran JumpCheck");
         }
+
+        else
+        {
+            rb.MovePosition(Vector2.MoveTowards(transform.position, destination, speed));
+            if (Vector2.Distance(transform.position, destination) < 4)
+            {
+                jumping = false;
+            }
+        }
+    }
+    */
+
+    [SerializeField]
+    private int jumpTimer;
+
+    [SerializeField]
+    private float speed;
+
+    public GameObject player;
+    public Rigidbody2D rb;
+
+    private Vector2 destination;
+    private enum states { NOTHING, SHOOTING, WAITING, JUMPING };
+    private states currentState;
+
+    void Start()
+    {
+        currentState = states.NOTHING;
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case states.NOTHING:
+
+
+            case states.SHOOTING:
+
+
+
+            case states.WAITING:
+                StartCoroutine(Jump());
+                break;
+
+            case states.JUMPING:
+                rb.MovePosition(Vector2.MoveTowards(transform.position, destination, speed));
+                if(Vector2.Distance(transform.position, destination) < 4)
+                {
+                    currentState = states.SHOOTING;
+                }
+                break;
+
+        }
+    }
+
+    private IEnumerator Jump()
+    {
+        currentState = states.WAITING;
+        yield return new WaitForSeconds(2);
+        destination = new Vector2(player.transform.position.x, player.transform.position.y);
+        currentState = states.JUMPING;
     }
 }
