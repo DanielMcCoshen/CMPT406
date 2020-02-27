23444e0d0db42ebfc7a2531354e39091e36a632c
Daniel McCoshen <upstagecoronet@gmail.com>
2020-02-27 01:06:44 -0600

    main menu mechanics. it still looks like shit though

---
+++ Unity/Assets/Resources/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class MainMenu : MonoBehaviour
+{
+    public string gameScene;
+    public TextMeshProUGUI rosterText;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void StartMenuUpdate()
+    {
+        StartCoroutine(UpdateRosterLoop());
+    }
+
+    private IEnumerator UpdateRosterLoop()
+    {
+        while (true)
+        {
+            string roster = "";
+            yield return new WaitForSeconds(1f);
+            Debug.Log(ServerInfo.Instance.Players);
+            foreach (PlayerInfo player in ServerInfo.Instance.Players)
+            {
+                    roster += player.name + '\n';
+            }
+           
+            rosterText.SetText(roster);
+        }
+    }
+}
---
+++ Unity/Assets/Resources/Scripts/NetworkManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NetworkManager
+{
+    private static readonly HttpClient client = new HttpClient();
+    public static async Task CreateGame()
+    {
+        HttpResponseMessage response;
+        RoomConfig json;
+        try
+        {
+            response = await client.PostAsync(ServerInfo.Instance.Hostname + "/game", null);
+            json = JsonUtility.FromJson<RoomConfig>(await response.Content.ReadAsStringAsync());
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e.Message);
+            return;
+        }
+        ServerInfo.Instance.RoomCode = json.room_id;
+        ServerInfo.Instance.Token = json.token;
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ServerInfo.Instance.Token);
+    }
+
+    public static async Task KillGame()
+    {
+        HttpResponseMessage response;
+        try
+        {
+            response = await client.DeleteAsync(ServerInfo.Instance.Hostname + "/game/" + ServerInfo.Instance.RoomCode);
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+
+    public static async Task UpdateRoomMembers()
+    {
+        HttpResponseMessage response;
+        PlayerList json;
+        try
+        {
+            response = await client.GetAsync(ServerInfo.Instance.Hostname + "/game/" + ServerInfo.Instance.RoomCode + "/users");
+            json = JsonUtility.FromJson<PlayerList>(await response.Content.ReadAsStringAsync());
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.Message);
+            return;
+        }
+        if (json.users != null)
+        {
+            ServerInfo.Instance.Players = json.users;
+        }
+        else
+        {
+            ServerInfo.Instance.Players = new List<PlayerInfo>();
+        }
+    }
+}
