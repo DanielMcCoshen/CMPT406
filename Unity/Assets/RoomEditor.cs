e9516d46bd2de5adda5175a8f9980390cd7a5913
Johann Doell (jsd090) <jsd090@mail.usask.ca>
2020-03-09 22:11:20 -0600

    Remade... all my rooms.

--- Unity/Assets/RoomEditor.cs
+++ Unity/Assets/RoomEditor.cs
@@ -1,40 +1,111 @@
-﻿using System.Collections;
+﻿//using System.Diagnostics;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class RoomEditor : MonoBehaviour {
+public class RoomEditor : MonoBehaviour
+{
+    public Transform tilemapObject;
 
-    [SerializeField]
-    Tilemap ground;
-    Tilemap walls;
-    Tilemap danger;
+    public Tilemap[] children;
 
-    void Start () {
-        FlipTilemap(ground);
+    void Start()
+    {
     }
 
-    public void FlipTilemap(Tilemap tilemapToFlip) {
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            FlipNSMaps();
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            FlipWEMaps();
+        }
+    }
+
+    void FlipNSMaps()
+    {
+        children = tilemapObject.GetComponentsInChildren<Tilemap>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (i != 1)
+            {
+                FlipTilemap(children[i], true);
+            }
+
+        }
+    }
+
+    void FlipWEMaps()
+    {
+        children = tilemapObject.GetComponentsInChildren<Tilemap>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (i != 1)
+            {
+                FlipTilemap(children[i], false);
+            }
+
+        }
+    }
+
+    public void FlipTilemap(Tilemap tilemapToFlip, bool rotateNS)
+    {
         Tilemap tilemap = tilemapToFlip;
+        tilemap.CompressBounds();
         BoundsInt bounds = tilemap.cellBounds;
         TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
 
         TileBase[] newTilebase = new TileBase[allTiles.Length];
 
-        Debug.Log("Flipping tilemap.");
+        //Debug.Log("Flipping tilemap.");
 
-        for (int x = 0; x < bounds.size.x; x++) {
-            for (int y = 0; y < bounds.size.y; y++) {
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
                 TileBase tile = allTiles[x + y * bounds.size.x];
                 //TileBase tile = tilemap.GetTile(new Vector3Int(x, y, 0));
-                if (tile != null) {
-                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
-                } else {
-                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                if (tile != null)
+                {
+                    //Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
                 }
-                int indexToAccess = (newTilebase.Length)-(x + (y * bounds.size.x));
-                Debug.Log(indexToAccess);
-                newTilebase[indexToAccess-1] = tile;
+                else
+                {
+                    //Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+
+                int indexToAccess = 0;
+
+                if (rotateNS)
+                {
+                    // Flip N-S
+                    //indexToAccess = (newTilebase.Length - 1) - ((x) + ((y * bounds.size.x)));
+                    indexToAccess = ((bounds.size.x - x - 1) + (((bounds.size.y - y - 1) * bounds.size.x)));
+                }
+                else
+                {
+                    // Flip W-E
+                    indexToAccess = ((x) + (((bounds.size.y - y - 1) * bounds.size.x)));
+
+                }
+
+                // Don't change
+                //indexToAccess = ((x) + ((y) * bounds.size.x));
+
+                //Debug.Log(indexToAccess);
+                try
+                {
+                    newTilebase[indexToAccess] = tile;
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Debug.LogError(indexToAccess);
+                }
             }
         }
         tilemapToFlip.SetTilesBlock(bounds, newTilebase);
