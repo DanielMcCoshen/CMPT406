b5a93d14a8de04bea70a0dc260d4c6fe334b14aa
Connor <cookeconnor98@gmail.com>
2020-04-02 22:53:34 -0600

    Began animation work

--- Unity/Assets/Resources/SpikePrototypeScrips/PlayerAnimations.cs
+++ Unity/Assets/Resources/SpikePrototypeScrips/PlayerAnimations.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerAnimations : EntityAnimation
 {
     public GameObject[] directionalObjects;
-    public int direction =0;
+    public Animator[] directionalAnimators;
+    public int direction = 0;
+    public int currentMovementDirection = -1;
 
     public Shooting weaponManager;
 
@@ -27,7 +29,7 @@ public class PlayerAnimations : EntityAnimation
         {
             weaponManager.CurrentWeapon().GetComponent<PlayerWeaponAnimations>().FaceNorth();
         }
-        
+        directionalAnimators[0].CrossFade("NorthFacingIdle", 0.1f, -1, 0.0f);
     }
 
     public override void FaceEast()
@@ -39,10 +41,12 @@ public class PlayerAnimations : EntityAnimation
         {
             weaponManager.CurrentWeapon().GetComponent<PlayerWeaponAnimations>().FaceEast();
         }
+        directionalAnimators[1].CrossFade("EastFacingIdle", 0.1f, -1, 0.0f);
     }
 
     public override void FaceSouth()
     {
+        
         directionalObjects[2].SetActive(true);
         directionalObjects[direction].SetActive(false);
         direction = 2;
@@ -50,10 +54,12 @@ public class PlayerAnimations : EntityAnimation
         {
             weaponManager.CurrentWeapon().GetComponent<PlayerWeaponAnimations>().FaceSouth();
         }
+        directionalAnimators[2].CrossFade("SouthFacingIdle", 0.1f, -1, 0.0f);
     }
 
     public override void FaceWest()
     {
+        
         directionalObjects[3].SetActive(true);
         directionalObjects[direction].SetActive(false);
         direction = 3;
@@ -61,12 +67,15 @@ public class PlayerAnimations : EntityAnimation
         {
             weaponManager.CurrentWeapon().GetComponent<PlayerWeaponAnimations>().FaceWest();
         }
+        directionalAnimators[3].CrossFade("WestFacingIdle", 0.1f, -1, 0.0f);
     }
 
     public override void CheckRotation()
     {
-        if (((aimPoint.rotation.eulerAngles.z >= 0 && aimPoint.rotation.eulerAngles.z <= 45) || (aimPoint.rotation.eulerAngles.z > 315 && aimPoint.rotation.eulerAngles.z < 360)) && direction != 0)
+        if (((aimPoint.rotation.eulerAngles.z >= 0 && aimPoint.rotation.eulerAngles.z <= 45) 
+            || (aimPoint.rotation.eulerAngles.z > 315 && aimPoint.rotation.eulerAngles.z < 360)) && direction != 0)
         {
+            Debug.Log(direction);
             FaceNorth();
         }
         else if (aimPoint.rotation.eulerAngles.z > 225 && aimPoint.rotation.eulerAngles.z <= 315 && direction != 1)
@@ -83,10 +92,41 @@ public class PlayerAnimations : EntityAnimation
         }
     }
 
+    private int UpdateMovementDirection()
+    {
+        if (Input.GetAxis("Horizontal") < Input.GetAxis("Vertical") && 0 < Input.GetAxis("Vertical"))
+        {
+            return 0;
+        }
+        else if (Input.GetAxis("Horizontal") > Input.GetAxis("Vertical") && 0 < Input.GetAxis("Horizontal"))
+        {
+            return 1;
+        }
+        else if (Input.GetAxis("Horizontal") > Input.GetAxis("Vertical") && 0 > Input.GetAxis("Vertical"))
+        {
+            return 2;
+        }
+        else if (Input.GetAxis("Horizontal") < Input.GetAxis("Vertical") && 0 > Input.GetAxis("Horizontal"))
+        {
+            return 3;
+        }
+        else
+        {
+            return -1;
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
         CheckRotation();
+        if(directionalObjects[direction].active)
+        {
+            int dir = UpdateMovementDirection();
+            Debug.Log(dir);
+            directionalAnimators[direction].SetInteger("CurrentDirection", dir);
+        }
 
     }
 }
