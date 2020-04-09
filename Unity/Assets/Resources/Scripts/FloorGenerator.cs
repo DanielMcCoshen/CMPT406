d8e6837d134c2209e30a239a6efd5853fe3ecf5c
Connor Cooke <59743777+ConnorCooke@users.noreply.github.com>
2020-04-09 17:27:48 -0600

    Update FloorGenerator.cs

--- Unity/Assets/Resources/Scripts/FloorGenerator.cs
+++ Unity/Assets/Resources/Scripts/FloorGenerator.cs
@@ -84,11 +84,7 @@ public class FloorGenerator : MonoBehaviour
             }
             else if (roomsTravelledPercent <= .666666667 && Random.Range(0.0f, 1.0f) >= roomsTravelledPercent + .30)
             {
-                Debug.Log(numberOfRooms);
-                Debug.Log(totalNumberOfRooms);
-                Debug.Log(roomsTravelledPercent);
                 bossRoomSpawned = true;
-                Debug.Log(true);
                 return true;
             }
             else
