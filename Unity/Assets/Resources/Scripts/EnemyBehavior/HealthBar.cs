9cada7b45ebf3a762a112d7ba0c70f9e5767aff1
Johann Doell (jsd090) <jsd090@mail.usask.ca>
2020-04-12 17:46:32 -0600

    Reorganized files.

---
+++ Unity/Assets/Resources/Scripts/EnemyBehavior/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Transform bar;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Vector3 heldPosOfBar = bar.transform.position;
+        bar.transform.position = new Vector3(heldPosOfBar.x, heldPosOfBar.y, 0);
+    }
+
+    // Update is called once per frame
+    public void SetSize(float sizeNormalized)
+    {
+        if(bar != null)
+        {
+            bar.localScale = new Vector3(sizeNormalized, 1f);
+        }
+        else
+        {
+            Debug.Log("HealthBar not found");
+        }
+    }
+}
