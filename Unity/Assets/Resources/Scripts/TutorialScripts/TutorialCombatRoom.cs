7e246be2011705b4d199a545442ca815ed12a0e8
Connor <cookeconnor98@gmail.com>
2020-04-15 21:06:27 -0600

    added tutorial combat rooms began work on their scripts

---
+++ Unity/Assets/Resources/Scripts/TutorialScripts/TutorialCombatRoom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCombatRoom : MonoBehaviour
+{
+
+    protected GameObject roomLayoutObj;
+    private bool reducedNumberOfRooms = false;
+
+    private bool enemiesDefeated = false;
+
+    public TutorialCombatRoomMap roomLayout;
+
+    void Start()
+    {
+    }
+
+
+
+    public void enterRoom()
+    {
+        if (!enemiesDefeated)
+        {
+            roomLayout.switchDynamicDanger(true);
+            roomLayout.spawnEnemies();
+        }
+    }
+
+    public void FixedUpdate()
+    {
+    }
+}
