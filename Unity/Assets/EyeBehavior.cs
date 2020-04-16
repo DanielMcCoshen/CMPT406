baff4427eea009d50b41a12dc222ba12cec493f5
Adam Soltys <ads576@mail.usask.ca>
2020-04-16 16:21:36 -0600

    Working on Eyes of Argus boss.

---
+++ Unity/Assets/EyeBehavior.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeBehavior : MonoBehaviour
+{
+    public GameObject head;
+
+    //Projectile prefabs
+    [SerializeField]
+    private GameObject purpleProjectile, redProjectile;
+
+    //Speed for projectiles in each state
+    [SerializeField]
+    private float normalSpeed, damagedSpeed, closedSpeed, redSpeed;
+
+    //Current state, determines attack cooldown and projectile speed
+    [HideInInspector]
+    public enum State { NORMAL, DAMAGED, CLOSED, RED }
+    private State currentState;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Starting state
+        currentState = State.NORMAL;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        switch (currentState)
+        {
+            case State.NORMAL:
+                if
+
+        }
+        */
+    }
+
+    private IEnumerator Shoot(float seconds, GameObject projectile, float projectileForce)
+        /**
+         * Method for firing projectiles
+         *      float seconds: How long to wait before firing
+         *      GameObject projectile: Which projectile to fire
+         *      float projectileForce: How fast the projectile should move
+         *      */
+    {
+        yield return new WaitForSeconds(seconds);
+        //Create the projectile
+        GameObject newProjectile = Instantiate(projectile, gameObject.transform);
+        //Get the rigidbody for the new projectile
+        Rigidbody2D rb = newProjectile.GetComponent<Rigidbody2D>();
+        //Add the appropriate force to that rigidbody
+        rb.AddForce(newProjectile.transform.up * projectileForce, ForceMode2D.Impulse);
+    }
+
+    public void ChangeState(State newState)
+        /**
+         * Method for changing the state of the eye
+         * */
+    {
+        currentState = newState;
+    }
+}
---
+++ Unity/Assets/HeadBehavior.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBehavior : MonoBehaviour
+{
+    //Current state of the boss, determines what the eyes should be doing
+    [HideInInspector]
+    public enum State { NORMAL, SHUDDERING, SPECIAL}
+    private State currentState;
+
+    //Tracking health
+    [SerializeField]
+    private float maxHealth;
+    private float currentHealth;
+
+    //Tracking the eyes on the boss
+    private List<EyeBehavior> eyes;
+    private List<EyeBehavior> activeEyes;
+    private List<EyeBehavior> inactiveEyes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set starting health
+        maxHealth = 1000.0f;
+        currentHealth = maxHealth;
+
+        //Set starting state
+        currentState = State.NORMAL;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddEye(EyeBehavior eye)
+        /**
+         * Method for adding eyes to the boss. This lets the boss control their states
+         * */
+    {
+        eyes.Add(eye);
+        activeEyes.Add(eye);
+    }
+
+    public float GetHealth()
+        /**
+         * Method for obtaining current health of the head
+         *      return: the current health of the head
+         *      */
+    {
+        return currentHealth;
+    }
+
+    public void SetHealth(float health)
+        /**
+         * Method for setting the current health value
+         *      float health: the value currentHealth should be set to
+         *      */
+    {
+
+        //Need to check if the new health value would be above the max health or below 0, and set the currentHealth accordingly
+        if (health >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else if (health <= 0.0f)
+        {
+            currentHealth = 0.0f;
+        }
+        else
+        {
+            if(currentHealth >= 500 && health < 500)
+            {
+                currentState = State.SHUDDERING;
+                Shudder(1.0f);
+            }
+            currentHealth = health;
+        }
+    }
+
+    public void SetState(State newState)
+        /**
+         * Set the current state of the boss
+         *      State newState: the new state for the boss
+         *      */
+    {
+        currentState = newState;
+    }
+
+    private IEnumerator Shudder(float seconds)
+    {
+        //Make the boss shudder for that many seconds
+        // [ADD CODE HERE]
+
+        //Set the state to the special attack state
+        yield return new WaitForSeconds(seconds);
+        SetState(State.SPECIAL);
+    }
+
+    public void DamageEye()
+        /**
+         * Method for randomly damaging one of the eyes (changing its state)
+         * */
+    {
+
+    }
+}
