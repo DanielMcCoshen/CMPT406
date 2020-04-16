ea4a4a2611226df275d21b465125c020ba83876b
Johann Doell (jsd090) <jsd090@mail.usask.ca>
2020-04-15 23:10:01 -0600

    Fixed player spawning issues in the boss fight.

---
+++ Unity/Assets/OnBossEnter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBossEnter : MonoBehaviour
+{
+
+    OnDeathTrapEnterPlayer playerDeathCollider;
+    string directoryToPlayerDeathCollider = "/PlayerCameraAndCanvas/PlayerContainer/DeathCollider";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDeathCollider = GameObject.Find(directoryToPlayerDeathCollider).GetComponent<OnDeathTrapEnterPlayer>();
+        playerDeathCollider.respawnPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
