cecc8e7f889de52f2bc48573361d8ea16fc36a00
Adam Soltys <ads576@mail.usask.ca>
2020-04-16 18:56:23 -0600

    Argus fires projectiles at player

---
+++ Unity/Assets/Resources/Prefabs/Enemies/Argus/EyeBehavior.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeBehavior : MonoBehaviour
+{
+    public HeadBehavior head;
+
+    //Projectile prefabs
+    [SerializeField]
+    private GameObject purpleProjectile, redProjectile;
+
+    //Speed for projectiles in each state
+    [SerializeField]
+    private float normalSpeed, damagedSpeed, closedSpeed, redSpeed;
+
+    //Current state, determines attack cooldown and projectile speed
+    [HideInInspector]
+    public enum State { NORMAL, DAMAGED, CLOSED, RED }
+    private State currentState;
+
+    //Used to see if it can fire another shot yet
+    private bool shooting;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Starting state
+        currentState = State.NORMAL;
+
+        //Add this eye to the central heads eye list
+        head = GameObject.FindWithTag("Head").GetComponent<HeadBehavior>();
+        if(head != null)
+        {
+            head.AddEye(this);
+        }
+        else
+        {
+            Debug.LogError("Head not found.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shooting == false)
+        {
+            switch (currentState)
+            {
+                //If the eye is normal
+                case State.NORMAL:
+                    //And the head is normal
+                    if (head.GetState() == HeadBehavior.State.NORMAL)
+                    {
+                        //And the random chance is good
+                        if (Random.Range(0.0f, 100.0f) <= 25.0f)
+                        {
+                            //Fire a shot
+                            shooting = true;
+                            StartCoroutine(Shoot(Random.Range(2.0f, 4.0f), purpleProjectile, normalSpeed));
+                        }
+                    }
+                    //If the head is shuddering
+                    else if (head.GetState() == HeadBehavior.State.SHUDDERING)
+                    {
+                        //Do nothing
+                        break;
+                    }
+                    //If the head is doing its special attack
+                    else if (head.GetState() == HeadBehavior.State.SPECIAL)
+                    {
+                        //And if the random chance is good
+                        if (Random.Range(0.0f, 100.0f) <= 25.0f)
+                        {
+                            //Fire a shot on a much lower cooldown
+                            shooting = true;
+                            StartCoroutine(Shoot(Random.Range(0.25f, 0.75f), purpleProjectile, normalSpeed));
+                        }
+                    }
+                    //If the head is in none of the above states
+                    else
+                    {
+                        //Something is wrong
+                        Debug.LogError("Invalid state");
+                    }
+                    break;
+
+                //If the eye is damaged
+                case State.DAMAGED:
+                    //And the head is normal
+                    if (head.GetState() == HeadBehavior.State.NORMAL)
+                    {
+                        //And the random chance is good
+                        if (Random.Range(0.0f, 100.0f) <= 25.0f)
+                        {
+                            //Fire a shot
+                            shooting = true;
+                            StartCoroutine(Shoot(Random.Range(3.0f, 5.0f), purpleProjectile, damagedSpeed));
+                        }
+                    }
+                    //If the head is shuddering
+                    else if (head.GetState() == HeadBehavior.State.SHUDDERING)
+                    {
+                        //Do nothing
+                        break;
+                    }
+                    //If the head is doing its special attack
+                    else if (head.GetState() == HeadBehavior.State.SPECIAL)
+                    {
+                        //And if the random chance is good
+                        if (Random.Range(0.0f, 100.0f) <= 25.0f)
+                        {
+                            //Fire a shot on a much lower cooldown
+                            shooting = true;
+                            StartCoroutine(Shoot(Random.Range(0.25f, 0.75f), purpleProjectile, damagedSpeed));
+                        }
+                    }
+                    //If the head is in none of the above states
+                    else
+                    {
+                        //Something is wrong
+                        Debug.LogError("Invalid state");
+                    }
+                    break;
+
+                //If the eye is red
+                case State.RED:
+                    //And the head is normal
+                    if (head.GetState() == HeadBehavior.State.NORMAL)
+                    {
+                        //And the random chance is good
+                        if (Random.Range(0.0f, 100.0f) <= 50.0f)
+                        {
+                            //Fire a shot
+                            shooting = true;
+                            StartCoroutine(Shoot(Random.Range(2.0f, 3.0f), redProjectile, redSpeed));
+                        }
+                    }
+                    //If the head is shuddering
+                    else if (head.GetState() == HeadBehavior.State.SHUDDERING)
+                    {
+                        //Do nothing
+                        break;
+                    }
+                    //If the head is doing its special attack
+                    else if (head.GetState() == HeadBehavior.State.SPECIAL)
+                    {
+                        //And if the random chance is good
+                        if (Random.Range(0.0f, 100.0f) <= 50.0f)
+                        {
+                            //Fire a shot on a much lower cooldown
+                            shooting = true;
+                            StartCoroutine(Shoot(Random.Range(0.25f, 0.75f), redProjectile, redSpeed));
+                        }
+                    }
+                    //If the head is in none of the above states
+                    else
+                    {
+                        //Something is wrong
+                        Debug.LogError("Invalid state");
+                    }
+                    break;
+
+                //If the eye is closed
+                case State.CLOSED:
+                    //Do nothing
+                    break;
+
+                default:
+                    Debug.LogError("Invalid state");
+                    break;
+            }
+        }
+        
+    }
+
+    private IEnumerator Shoot(float seconds, GameObject projectile, float projectileForce)
+        /**
+         * Method for firing projectiles
+         *      float seconds: How long to wait before firing
+         *      GameObject projectile: Which projectile to fire
+         *      float projectileForce: How fast the projectile should move
+         *      */
+    {
+        yield return new WaitForSeconds(seconds);
+        //Create the projectile
+        GameObject newProjectile = Instantiate(projectile, gameObject.GetComponent<AimAtPlayer>().firePoint.transform.position, gameObject.GetComponent<AimAtPlayer>().firePoint.transform.rotation);
+        //Get the rigidbody for the new projectile
+        Rigidbody2D rb = newProjectile.GetComponent<Rigidbody2D>();
+        //Add the appropriate force to that rigidbody
+        rb.AddForce(newProjectile.transform.up * projectileForce, ForceMode2D.Impulse);
+
+        shooting = false;
+    }
+
+    public void SetState(State newState)
+        /**
+         * Method for changing the state of the eye
+         * */
+    {
+        currentState = newState;
+    }
+
+    public State GetCurrentState()
+        /**
+         * Method to get the current state of the eye
+         * */
+    {
+        return currentState;
+    }
+}
---
+++ Unity/Assets/Resources/Prefabs/Enemies/Argus/HeadBehavior.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBehavior : MonoBehaviour
+{
+    //Current state of the boss, determines what the eyes should be doing
+    [HideInInspector]
+    public enum State { NORMAL, SHUDDERING, SPECIAL}
+    private State currentState;
+
+    //Tracking health
+    [SerializeField]
+    private float maxHealth;
+    private float currentHealth;
+
+    //Tracking the eyes on the boss
+    private List<EyeBehavior> eyes = new List<EyeBehavior>();
+    private List<EyeBehavior> activeEyes = new List<EyeBehavior>();
+    private List<EyeBehavior> inactiveEyes = new List<EyeBehavior>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set starting health
+        maxHealth = 1000.0f;
+        currentHealth = maxHealth;
+
+        //Set starting state
+        currentState = State.NORMAL;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddEye(EyeBehavior eye)
+        /**
+         * Method for adding eyes to the boss. This lets the boss control their states
+         * */
+    {
+        eyes.Add(eye);
+        activeEyes.Add(eye);
+    }
+
+    public float GetHealth()
+        /**
+         * Method for obtaining current health of the head
+         *      return: the current health of the head
+         *      */
+    {
+        return currentHealth;
+    }
+
+    public void SetHealth(float health)
+        /**
+         * Method for setting the current health value
+         *      float health: the value currentHealth should be set to
+         *      */
+    {
+        //Need to check if the new health value would be above the max health or below 0, and set the currentHealth accordingly
+        if (health >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else if (health <= 0.0f)
+        {
+            currentHealth = 0.0f;
+        }
+        else
+        {
+            //Checks for if the boss should do its threshold attacks
+            if(currentHealth >= 500.0f && health < 500.0f)
+            {
+                currentState = State.SHUDDERING;
+                SpecialAttack(1.0f, 4.0f);
+            }
+            else if(currentHealth >= 200.0f && health < 200.0f)
+            {
+                currentState = State.SHUDDERING;
+                SpecialAttack(1.5f, 6.0f);
+            }
+            currentHealth = health;
+        }
+    }
+
+    public void SetState(State newState)
+        /**
+         * Set the current state of the boss
+         *      State newState: the new state for the boss
+         *      */
+    {
+        currentState = newState;
+    }
+
+    public State GetState()
+        /**
+         * Method for getting the current state of the boss
+         *      return: the current state of the boss
+         * */
+    {
+        return currentState;
+    }
+
+    private IEnumerator SpecialAttack(float shudderSeconds, float attackSeconds)
+    {
+        /**
+         * Method for making the boss do its special attack. 
+         *      float shudderSeconds: how long the boss shudders before attacking
+         *      float attackSeconds: How long the boss should do the attack for
+         *      */
+        yield return new WaitForSeconds(shudderSeconds);
+        SetState(State.SPECIAL);
+        yield return new WaitForSeconds(attackSeconds);
+        SetState(State.NORMAL);
+
+    }
+
+    public void DamageEye()
+        /**
+         * Method for randomly damaging one of the eyes (changing its state)
+         * */
+    {
+        //Randomly select an active eye and move it to the next state of damage. Put it in the appropriate list (damaged to destroyed means it must be removed from the active eyes so it can't be damaged again)
+        EyeBehavior selectedEye = activeEyes[Random.Range(0, activeEyes.Count)];
+        switch(selectedEye.GetCurrentState())
+        {
+            case EyeBehavior.State.NORMAL:
+                selectedEye.SetState(EyeBehavior.State.DAMAGED);
+                break;
+            case EyeBehavior.State.DAMAGED:
+                selectedEye.SetState(EyeBehavior.State.CLOSED);
+                activeEyes.Remove(selectedEye);
+                inactiveEyes.Add(selectedEye);
+                break;
+            case EyeBehavior.State.RED:
+                selectedEye.SetState(EyeBehavior.State.CLOSED);
+                activeEyes.Remove(selectedEye);
+                inactiveEyes.Add(selectedEye);
+                break;
+            default:
+                Debug.LogError("Invalid state.");
+                break;
+        }
+    }
+
+    public void AwakeEyes()
+        /**
+         * Method for resurrecting all destroyed eyes as red eyes
+         * */
+    {
+        foreach(EyeBehavior eye in inactiveEyes)
+        {
+            inactiveEyes.Remove(eye);
+            activeEyes.Add(eye);
+            eye.SetState(EyeBehavior.State.RED);
+        }
+    }
+}
